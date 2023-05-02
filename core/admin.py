a74288100e2af77fe2a9161c939680d2ede89061
TheLordVier <messi060591@gmail.com>
2023-05-02 15:33:52 +0300

    update commit

---
+++ core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
+from core.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('email', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.unregister(Group)
