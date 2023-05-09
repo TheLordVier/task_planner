3c2c417400a0d257fa1db15709e26fa416c0e868
TheLordVier <messi060591@gmail.com>
2023-05-09 12:18:11 +0300

    update commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-python manage.py migrate --check
+python ./manage.py migrate --check
 status=$?
 if [[ status != 0 ]]; then
-  python manage.py migrate
+  python ./manage.py migrate
 fi
-python manage.py collectstatic -c --no-input
+python ./manage.py collectstatic -c --no-input
 exec "$@"
