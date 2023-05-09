6e24ec5302a328a02b935de36f33b3a89fc7a90d
TheLordVier <messi060591@gmail.com>
2023-05-09 11:46:39 +0300

    update commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -132,5 +132,3 @@ STATIC_ROOT = BASE_DIR.joinpath('static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#AUTH_USER_MODEL = 'core.User'
